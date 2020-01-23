4c108748c6ffea997c0e7c2eac670dcf10f77372
Shane Geary <stg4462@comcast.net>
2020-01-23 22:46:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   ["Dubstep", "Trap", "Dnb", "House"]
-  "Grime" = array.push 
+  "Grime"array.push 
 end
 
 def add_element_to_start_of_array(array, element)
